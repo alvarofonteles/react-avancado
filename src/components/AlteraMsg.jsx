33bac6a16b54af638aba17ebe6a14e60f7e5ef19
Alvaro <alvarofonteles@hotmail.com>
2025-05-22 15:04:18 -0300

    fim

--- src/components/AlteraMsg.jsx
+++ src/components/AlteraMsg.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AlteraMsg = ({ stNm }) => {
 
     const valorAlterado = ['Oi', 'Olá', 'Tudo Bem']
+    const [inputValue, setInputValue] = useState('')
 
     return (
         <>
             <button onClick={() => stNm(valorAlterado[0])} >1</button>
             <button onClick={() => stNm(valorAlterado[1])} >2</button>
             <button onClick={() => stNm(valorAlterado[2])} >3</button>
+
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') stNm(inputValue);
+                }}
+            />
+
+            <p />
+
+            <button onClick={() => stNm(inputValue)} >Enter</button>
+            <p>Nome alterado: {inputValue}</p>
         </>
     )
 }
